4dca32fea3d9ac859a9d57caf2960d6e025de45d
Guillermo leaño <34119019+guillermoleano@users.noreply.github.com>
2026-03-20 17:34:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+order_created = ()
+import time
+from utils import function1_client_registration 
+from utils import function2_product_registration
+from utils import function3_order_creation
+from utils import function4_consulting_order_registered
+from utils import function5_daily_revenue_calculation
+from utils import function6_final_report_generation
+
+print("Welcome to the Customer Order Management System")
+
+while True:
+    option = int(input("""
+    Please select an option:
+    --------------------------------
+    1. Create orders
+    2. Consult orders
+    3. Daily Revenue Calculation
+    4. Final Report                
+    5. Exit
+    --------------------------------
+    """))
+    
+    if option == 1:
+        client = function1_client_registration()
+        print(f"Success! Customer {client['client_name']} registered.")  
+        product = function2_product_registration()
+        print(f"Success! Product  registered for {client['client_name']} .")
+        total = function3_order_creation(client, product)
+        #print(f"Success! Order created for {client['client_name'] with total: {total}")
+        order_created = order_created + (client, product, total)
+        #print(order_created)
+    if option == 2:
+        function4_consulting_order_registered(order_created)
+    if option == 3:
+        revenue = function5_daily_revenue_calculation(order_created)
+    if option == 4:
+        function6_final_report_generation(order_created)
+    #if option == "5":
+    #    print("Goodbye!")"""
+    
---
+++ utils.py
@@ -0,0 +1,120 @@
+
+def function1_client_registration():
+    print("\n--- Register New Customer ---")
+    client_id = input("Enter customer ID: ")
+    client_name = input("Enter customer name: ")
+    
+    while True:
+        email = input("Enter customer email: ")
+        if "@" in email:
+            break 
+        else:
+            print("Error: Your email must have an @ to be registered.")
+
+    
+    client_registration = {
+        "client_name": client_name,
+        "client_email": email,
+        "client_id": client_id,
+    }    
+    return client_registration
+def function2_product_registration():
+    product_name = input("Enter product name: ")
+    
+    try:
+        unit_price = float(input("Enter unit price: "))
+    except ValueError:
+        print("Invalid price. Using 0.0")
+        price = 0.0
+    
+    
+    product_id = int(input("Enter product ID: "))
+    quantity = int(input("Enter quantity: ")) 
+
+    
+    product_registration = (product_id, product_name, unit_price, quantity) 
+    return product_registration
+def function3_order_creation(client, product):
+    #print(product[2])  # ver variable unit_price
+    #print(product[3])   # ver variable quantity
+    unit_price =product[2] # buscar valor de la tupla (unit_price)en posicion product[2] y añadir a variable unit_price
+    quantity = product[3] # buscar valor de la tupla (quantity)en posicion product[3] y añadir a variable quantity
+    order_total = unit_price * quantity
+    #print(order_total) #test
+
+    return order_total
+def function4_consulting_order_registered(order_created):
+    #print(order_created)  
+    
+    for i in range(0, len(order_created), 3):
+        client = order_created[i]
+        product = order_created[i+1]
+        total = order_created[i+2]
+        print("-" * 30)
+        print("Client:", client)
+        print("Product:", product)
+        print("Total:", total)
+        print("-" * 30)
+def function5_daily_revenue_calculation(order_created):
+    print(order_created)
+    sum_total = 0
+
+    for i in range(0, len(order_created), 3):
+        total = order_created[i+2]
+        sum_total += total   # to accumulate the total of revenue
+    print("Total revenue:", sum_total)
+    return sum_total
+def function6_final_report_generation(order_created):
+
+    # <-- here prints the number of orders
+    print(f"Number of orders: {int(len(order_created)/3)}") 
+
+    # <-- here prints the total revenue
+    sum_final_report = 0
+    for i in range(0, len(order_created), 3):
+        total = order_created[i+2]
+        sum_final_report += total   # <-- aquí acumulas
+    print("Total revenue:", sum_final_report)
+
+
+    for i in range(0, len(order_created), 3):
+        producto = order_created[i+1][1]
+        print(f"Product: {producto} - Quantity: {order_created[i+1][3]}") # <-- aqui imprimes el producto)
+
+    print(order_created)
+
+    reporte = {}
+
+    for i in range(0, len(order_created), 3):
+        producto = order_created[i+1][1]
+        cantidad = order_created[i+1][3]
+        reporte[producto] = reporte.get(producto, 0) + cantidad
+    print(reporte)
+
+    #------------------PRINT ORDER GROUPED BY CLIENT
+    pedidos_por_cliente = {}
+
+    for i in range(0, len(order_created), 3):
+        cliente = order_created[i]
+        producto = order_created[i+1]
+        total = order_created[i+2]
+
+        nombre = cliente['client_name']
+
+        if nombre not in pedidos_por_cliente:
+            pedidos_por_cliente[nombre] = {
+                "productos": []
+            }
+
+        pedidos_por_cliente[nombre]["productos"].append({
+            "producto": producto[1],
+            "cantidad": producto[3],
+            "total": total
+        })
+
+    # Imprimir pedidos agrupados
+    for cliente, info in pedidos_por_cliente.items():
+        print(f"\nClient: {cliente}" )
+        print("Associated products:")
+        for p in info["productos"]:
+            print(f"  - {p['producto']} | Cantidad: {p['cantidad']} | Total: {p['total']}")
