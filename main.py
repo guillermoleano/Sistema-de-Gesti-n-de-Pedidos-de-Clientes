e4b145fa4f19698762f60a2df6cc656f80178664
Guillermo leaño <34119019+guillermoleano@users.noreply.github.com>
2026-03-19 23:19:09 -0500

    Update main.py
    correcion

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import function1_client_registration from utils
-import function2_product_registration from utils
-import function3_order_creation from utils
-import function4_consulting_order_registered from utils
-import function5_daily_revenue_calculation from utils
-import function6_final_report_generation from utils
-
+import time
+from utils import function1_client_registration 
+from utils import function2_product_registration
+from utils import function3_order_creation
+from utils import function4_consulting_order_registered
+from utils import function5_daily_revenue_calculation
+from utils import function6_final_report_generation
 
 print("Welcome to the Customer Order Management System")
 
@@ -18,12 +18,12 @@ while = True:
     4. Exit
     --------------------------------
     """)
-    if option == "1":
+    if option == 1:
         client = function1_client_registration()
         product = function2_product_registration()
-        order_total, quantity = function3_order_creation(client, product)
-    if option == "2":
-        function4_consulting_order_registered(client, product, quantity, order_total)
+        total = function3_order_creation(client, product)
+    if option == 2:
+        function4_consulting_order_registered(client, product, total)
         function5_daily_revenue_calculation()
     if option == "3":
         function6_final_report_generation()
